4dc9a9309ecf94a24ee7adec8d77714c0403f78d
susmitabhowmik <susmita.bhowmik516@gmail.com>
2020-01-14 14:11:02 -0500

    :bug: Update Script For Linux
    Xargs gives an error in Linux for words with apostrophes because xargs is searching for a closing quotation. To get around this error, add the words after they are filtered into touch as an argument. Also add in ls so the user can see new files created.

--- words.sh
+++ words.sh
@@ -1,3 +1,6 @@
-#grep searches the file for all the words beginning with A regardless of casing, tail takes the last 10 words, and xargs adds the touch command to all the words separated by whitespace. 
-grep ^a -E -i /usr/share/dict/words | tail | xargs touch
-echo done
+#grep searches the file for all the words beginning with A regardless of casing, tail takes the last 10 words
+#touch will create new files separated by white space
+#used touch instead of piping in xargs touch because xargs gives an error in linux dictionary for words what have an appostrophe
+
+touch $(grep ^a -E -i /usr/share/dict/words | tail) 
+ls
