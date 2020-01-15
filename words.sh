acdf2f1c90c62af176775bb421c42dba9b2726ab
susmitabhowmik <susmita.bhowmik516@gmail.com>
2020-01-15 10:40:05 -0500

    Feat: Enable user to create a new directory to add in files User can name and create a directory to add files into, instead of files being created in the current directory. If the user wishes to remove these files later, they can remove the directory created.

--- words.sh
+++ words.sh
@@ -2,5 +2,9 @@
 #touch will create new files separated by white space
 #used touch instead of piping in xargs touch because xargs gives an error in linux dictionary for words what have an appostrophe
 
+echo "Please name the directory you would like to create:":
+read directory
+mkdir ${directory}
+cd ${directory}
 touch $(grep ^a -E -i /usr/share/dict/words | tail) 
 ls
